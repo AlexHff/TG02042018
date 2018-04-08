28ccb1dc7c1010542b2bbbd05816eaa6deda3f5d
alexhff <alexander.hoffmann@edu.ece.fr>
2018-04-08 10:50:09 +0200

    v6

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int main()
     g3.read_file_del();
 
     workg=g3;
-   // workg.kVertexConnex();
+    workg.kVertexConnex();
 
     /// Vous gardez la main sur la "boucle de jeu"
     /// ( contrairement à des frameworks plus avancés )
